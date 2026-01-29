3574ae4f0d9f941a7a150caff4a78725797ce639
Muhammad Hassan Azmat <mhazmatjunior@gmail.com>
2026-01-29 19:16:45 +0500

    Finished Project
    It is completed using app router <3
    Meals Images will not load in production as i have not deployed them, but they will work in development server as they are in public folder path right now !

---
+++ components/meals/image-picker.js
@@ -0,0 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const imageInputRef = useRef();
+  const [pickedImage, setPickedImage] = useState(null);
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  function handlePickImage() {
+    imageInputRef.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="Picked Image of User" fill />
+          )}
+        </div>
+        <input
+          ref={imageInputRef}
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          onClick={handlePickImage}
+          type="button"
+          className={classes.button}
+        >
+          Pick Image
+        </button>
+      </div>
+    </div>
+  );
+}
